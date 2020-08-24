8a3668771d99f77b0bcf082698314e4ee241a184
Francinildo M. Rodrigues <francis584@gmail.com>
2020-08-23 21:47:54 -0300

    list transactions done

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,15 +1,26 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
-// transactionsRouter.get('/', async (request, response) => {
-//   // TODO
-// });
+transactionsRouter.get('/', async (request, response) => {
+  // TODO
+  const transactionRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionRepository.find({
+    relations: ['category'],
+    select: ['id', 'title', 'value', 'type', 'created_at', 'updated_at'],
+  });
+
+  const balance = await transactionRepository.getBalance();
+
+  return response.json({ transactions, balance });
+});
 
 transactionsRouter.post('/', async (request, response) => {
   // TODO
